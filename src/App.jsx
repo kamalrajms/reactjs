d6b0faefc6816e04675edb4380cc62dbf130b773
kamalrajms <kamalrajindtn@gmail.com>
2025-09-01 16:08:28 +0530

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import React, { useState, createContext, useContext } from "react";
-import Apple from "./components/Apple";
-
-export const Pass = createContext();
+import React, { useState, createContext, useContext, useReducer } from "react";
 
 export default function App() {
-  const name = "animal";
+  // const [count, setcount] = useState(0);
+
+  const initialState = { count: 0 };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "inc":
+        return { count: state.count + 1 };
+      case "dec":
+        return { count: state.count - 1 };
+      case "reset":
+        return { count: 0 };
+      default:
+        return { count: 0 };
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
-      <Pass.Provider value={name}>
-        <Apple />
-      </Pass.Provider>
+      {/* {count}
+      <button onClick={() => setcount(count + 1)}>inc</button>
+      <br />
+      <button onClick={() => setcount(count - 1)}>dec</button>
+      <br />
+      <button onClick={() => setcount(0)}>reset</button> */}
+
+      <h1>count :{state.count}</h1>
+      <button onClick={() => dispatch({ type: "inc" })}>+</button>
+      <br />
+      <button onClick={() => dispatch({ type: "dec" })}>-</button>
+      <br />
+      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
     </div>
   );
 }
--- src/components/Cat.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useContext } from "react";
-import { Pass } from "../App";
-
-export default function Cat() {
-  const passname = useContext(Pass);
-  return <div>Cat copmponents {passname}</div>;
-}
